251ce2fd9e268fff73f03f18367e59302eb4c5ff
Jeremy Hernández <jeremy2437@gmail.com>
2017-01-18 01:58:45 +0100

    feat(refacto&cursor): refact some code from GameState.js, and first implementation of player movments

---
+++ src/objects/CursorsManager.js
@@ -0,0 +1,5 @@
+export default class {
+  constructor(game) {
+    return game.input.keyboard.createCursorKeys();
+  }
+}
---
+++ src/objects/Loader.js
@@ -0,0 +1,7 @@
+export default class {
+  constructor(loader) {
+      loader.tilemap('tilemap', 'json/level1.json', null, Phaser.Tilemap.TILED_JSON);
+      loader.image('NESSuper3', 'images/NESSuper3.png');
+      loader.atlasJSONHash('mario', '../images/mario.png', '../json/mario.json');
+  }
+}
